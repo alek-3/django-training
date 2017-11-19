b7c922b83570abd6625d53c776a4d0920cd13115
alek-3 <jaianism.of.grotius@hotmail.co.jp>
2017-11-19 15:58:13 +0900

    (4) 追加・修正のフォームを作成

--- cms/views.py
+++ cms/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
+from cms.forms import BookForm
 from cms.models import Book
 
 
@@ -12,9 +13,26 @@ def book_list(request):
                   'cms/book_list.html',  # 使用するテンプレート
                   {'books': books})        # テンプレートに渡すデータ
 
+
 def book_edit(request, book_id=None):
     """書籍の編集"""
-    return HttpResponse('書籍の編集')
+    # return HttpResponse('書籍の編集')
+    if book_id: # book_idが指定されているとき（修正時）
+        book = get_object_or_404() # get_object_or_404はDjangoが用意してくれるクラス
+    else: # 追加時
+        book = Book()
+
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book) #postされたrequestデータからフォームを作成
+        if form.is_valid(): # is_validはDjangoが勝手に生成していた
+            book = form.save(commit=False)
+            book.save()
+            return redirect('cms:book_list')
+    else:
+        form = BookForm(instance=book) # bookインスタンスからフォームを作成
+
+    return render(request, 'cms/book_edit.html', dict(form=form, book_id=book_id))
+
 
 def book_del(request, book_id):
     """書籍の削除"""
